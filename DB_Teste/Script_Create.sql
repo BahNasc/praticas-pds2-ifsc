6bd05aac3964d9145eb670115f1fb5d230d544e3
Bárbara <barbara.n2005@aluno.ifsc.edu.br>
2023-08-22 07:38:09 -0300

    projeto finalizado

---
+++ DB_Teste/Script_Create.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE IF NOT EXISTS banco_teste;
+
+USE banco_teste;
+
+CREATE TABLE IF NOT EXISTS `pessoa` (
+  `id` INT AUTO_INCREMENT NOT NULL,
+  `primeiro_nome` VARCHAR(45) NOT NULL,
+  `idade` INT NOT NULL,
+  PRIMARY KEY (`id`)
+);
