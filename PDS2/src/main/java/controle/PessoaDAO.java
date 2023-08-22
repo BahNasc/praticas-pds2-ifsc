39d768cf41bbc5ec34ac27a4aeb98ee5e1d3b226
Bárbara <barbara.n2005@aluno.ifsc.edu.br>
2023-08-22 08:45:05 -0300

    Aula 3 - Parte 2

--- PDS2/src/main/java/controle/PessoaDAO.java
+++ PDS2/src/main/java/controle/PessoaDAO.java
@@ -43,5 +43,47 @@ public class PessoaDAO {
 		}
 		return false;
 	}
+	
+	public boolean excluir(Pessoa p) {
+		Conexao c = Conexao.getInstancia();
+		Connection con = c.conectar();
+		
+		String query = "DELETE FROM pessoa WHERE id = ?";
+		try {
+			PreparedStatement ps = con.prepareStatement(query);
+			ps.setInt(1, p.getId());
+			ps.executeUpdate();
+			
+			c.fecharConexao();
+			
+			return true;
+		} catch (SQLException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+				return false;
+	}
+	
+	public boolean atualizar(Pessoa p) {
+		Conexao c = Conexao.getInstancia();
+		Connection con = c.conectar();
+		
+		String query = "UPDATE pessoa SET " + "primeiro_nome = ? WHERE id_pessoa =?";
+		try {
+			PreparedStatement ps = con.prepareStatement(query);
+			ps.setString(1, p.getPrimeiro_nome());
+			ps.setInt(2, p.getId());
+			
+			ps.executeUpdate();
+			c.fecharConexao();
+			
+			return true;
+		} catch (SQLException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		return false;
+	}
+	
 
 }
